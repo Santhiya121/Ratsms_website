02631fdc77c06ebe1d94247340cbc48dda3cb824
Santhiya121 <santhiyar121@gmail.com>
2025-02-04 20:13:07 +0530

    404 updated

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Custom404() {
-  return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <Link href="/">Go Back Home</Link>
-    </div>
-  );
+  redirect("/");
 }
